86389e1d8b948a66f85e8b4af7bb35938b153494
Jassen <jassenlyou@foxmail.com>
2023-05-09 23:15:58 +0800

    feat: handle login

--- src/components/WalletMenu/index.js
+++ src/components/WalletMenu/index.js
@@ -1,13 +1,15 @@
 import { LoadingButton } from '@mui/lab';
 import { useConnectModal } from '@rainbow-me/rainbowkit';
-import { memo, useState } from 'react';
-import { useAccount, useDisconnect } from 'wagmi';
+import { memo, useCallback, useEffect, useState } from 'react';
+import { useAccount, useDisconnect, useSigner } from 'wagmi';
 import { splitAddress } from '../../utils';
 import { ProfileIcon, ExitIcon } from '../../assets';
 import { ListItemText, Menu, MenuItem, styled } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import CommonAvatar from '../CommonAvatar';
-import { useSelector } from 'react-redux';
+import { login } from '../../api/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const WalletItem = styled(MenuItem)(({ theme }) => ({
 	...theme.typography.caption,
@@ -19,11 +21,15 @@ const WalletItem = styled(MenuItem)(({ theme }) => ({
 const ConnectWallet = () => {
 	const { openConnectModal } = useConnectModal();
 	const { disconnect } = useDisconnect();
-	const { address } = useAccount();
+	const { address, isConnected } = useAccount();
+	const { data: singer, isLoading, isSuccess } = useSigner();
+
+	const dispatch = useDispatch();
+
 	const { profileInfo } = useSelector((state) => ({
 		profileInfo: state.reflect_loginInfo,
 	}));
-	// const { data } = useEnsAvatar({ address });
+
 	const [anchorEl, setAnchorEl] = useState(null);
 	const navigate = useNavigate();
 	const open = Boolean(anchorEl);
@@ -31,19 +37,60 @@ const ConnectWallet = () => {
 	const handleClick = (event) => {
 		setAnchorEl(event.currentTarget);
 	};
-	const handleClose = () => {
+	const handleClose = useCallback(() => {
 		setAnchorEl(null);
-	};
+	}, []);
 
 	const handleClickProfile = () => {
 		navigate('/profile');
 		handleClose();
 	};
 
-	const disconnectWallet = () => {
+	const disconnectWallet = useCallback(() => {
 		disconnect();
 		handleClose();
-	};
+		dispatch({ type: 'LOGOUT' });
+	}, [dispatch, disconnect, handleClose]);
+
+	const handleLogin = useCallback(
+		async (singer) => {
+			try {
+				if (singer?._isSigner) {
+					const signInfo = await singer.signMessage(address);
+					console.log('signInfo:', signInfo);
+					const resp = await login({
+						address: address,
+						message: address,
+						signature: signInfo,
+					});
+					if (resp?.code === 0 && resp?.data && resp.data.access_token) {
+						dispatch({ type: 'SET_TOKEN', value: resp.data.access_token });
+						dispatch({ type: 'SET_PRE_ACCOUNT', value: address });
+					}
+					console.log('loginResp:', resp);
+				}
+			} catch (error) {
+				console.log('error:', error);
+				if (
+					error?.code === 4001 &&
+					error.message.includes('User denied message signature')
+				) {
+					toast.error(error.message);
+				}
+				disconnectWallet();
+			}
+		},
+		[address, disconnectWallet, dispatch]
+	);
+
+	useEffect(() => {
+		if (singer && isSuccess && !isLoading && isConnected) {
+			singer.getAddress().then((_account) => {
+				if (_account !== profileInfo.preAccount) handleLogin(singer);
+			});
+		}
+		// eslint-disable-next-line
+	}, [isConnected, singer, isSuccess, isLoading]);
 
 	return (
 		<div>
