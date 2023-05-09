f40b97fe91755eced57484be5aa544f3fbb4651e
Jassen <jassenlyou@foxmail.com>
2023-05-09 22:38:53 +0800

    feat: import store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiConfig } from 'wagmi';
 import { chains, wagmiClient } from './config/client';
+import { Provider } from 'react-redux';
 
 import './index.less';
 import '@rainbow-me/rainbowkit/styles.css';
@@ -12,18 +13,21 @@ import 'react-step-progress-bar/styles.css';
 import AuthRouter from './provider/AuthRouter';
 import { ThemeProvider } from '@mui/material';
 import theme from './config/theme';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
-		<ThemeProvider theme={theme}>
-			<WagmiConfig client={wagmiClient}>
-				<RainbowKitProvider chains={chains}>
-					<AuthRouter>
-						<App />
-					</AuthRouter>
-				</RainbowKitProvider>
-			</WagmiConfig>
-		</ThemeProvider>
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<WagmiConfig client={wagmiClient}>
+					<RainbowKitProvider chains={chains}>
+						<AuthRouter>
+							<App />
+						</AuthRouter>
+					</RainbowKitProvider>
+				</WagmiConfig>
+			</ThemeProvider>
+		</BrowserRouter>
+	</Provider>
 );
